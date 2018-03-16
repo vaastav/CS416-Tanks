3568535e9788d14efd56d2a7ca635adcd370f350
Jerome Rasky <jyrome.112@gmail.com>
2018-03-16 13:49:26 -0700

    Make some objects

--- client/client.go
+++ client/client.go
@@ -13,28 +13,63 @@ import (
 	"time"
 )
 
-func run() {
-	cfg := pixelgl.WindowConfig{
+type Player struct {
+	sprite *pixel.Sprite
+	ID uint64
+	Pos pixel.Vec
+	Angle float64
+}
+
+func NewPlayer() *Player {
+	return &Player {
+		ID: rand.Uint64(),
+		sprite: pixel.NewSprite(playerPic, playerPic.Bounds()),
+	}
+}
+
+func (p *Player) Draw(t pixel.Target) {
+	mat := pixel.IM.Scaled(pixel.ZV, 0.25).
+		Rotated(pixel.ZV, p.Angle).Moved(p.Pos)
+
+	p.sprite.Draw(t, mat)
+}
+
+var (
+	localPlayer *Player
+	windowCfg = pixelgl.WindowConfig{
 		Title: "Wednesday",
 		Bounds: pixel.R(0, 0, 1024, 768),
 		VSync: true,
 	}
+	playerPic pixel.Picture
+	players []*Player
+)
 
-	win, err := pixelgl.NewWindow(cfg)
+const (
+	playerSpeed = 150.0
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	// Load the player picture
+	var err error
+	playerPic, err = loadPicture("images/player.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	playerPic, err := loadPicture("images/player.png")
+	localPlayer = NewPlayer()
+	localPlayer.Pos = windowCfg.Bounds.Center()
+
+	pixelgl.Run(run)
+}
+
+func run() {
+	win, err := pixelgl.NewWindow(windowCfg)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	player := pixel.NewSprite(playerPic, playerPic.Bounds())
-	playerMat := pixel.IM.Scaled(pixel.ZV, 0.5).Moved(win.Bounds().Center())
-	playerPos := win.Bounds().Center().Scaled(0.5)
-
-	const playerSpeed = 150.0
 
 	win.SetSmooth(true)
 
@@ -46,46 +81,38 @@ func run() {
 		last = time.Now()
 
 		if win.Pressed(pixelgl.KeyA) {
-			playerPos.X -= playerSpeed * dt
+			localPlayer.Pos.X -= playerSpeed * dt
 		}
 
 		if win.Pressed(pixelgl.KeyD) {
-			playerPos.X += playerSpeed * dt
+			localPlayer.Pos.X += playerSpeed * dt
 		}
 
 		if win.Pressed(pixelgl.KeyS) {
-			playerPos.Y -= playerSpeed * dt
+			localPlayer.Pos.Y -= playerSpeed * dt
 		}
 
 		if win.Pressed(pixelgl.KeyW) {
-			playerPos.Y += playerSpeed * dt
+			localPlayer.Pos.Y += playerSpeed * dt
 		}
 
-		mousePosition := win.MousePosition().Sub(playerPos)
+		mousePosition := win.MousePosition().Sub(localPlayer.Pos)
 
 		imd.Clear()
 
 		lineLength := win.Bounds().Max.Sub(win.Bounds().Min).Len()
 
-		endPoint := mousePosition.Scaled(lineLength / mousePosition.Len()).Add(playerPos)
+		endPoint := mousePosition.Scaled(lineLength / mousePosition.Len()).Add(localPlayer.Pos)
 
 		imd.Color = colornames.Darkred
-		imd.Push(playerPos, endPoint)
+		imd.Push(localPlayer.Pos, endPoint)
 		imd.Line(3)
-
-		angle := mousePosition.Angle()
-		playerMat = pixel.IM.Scaled(pixel.ZV, 0.25).Rotated(pixel.ZV, angle).Moved(playerPos)
 
 		win.Clear(colornames.Whitesmoke)
 		imd.Draw(win)
-		player.Draw(win, playerMat)
+		localPlayer.Draw(win)
 		win.Update()
 	}
-}
-
-func main() {
-	rand.Seed(time.Now().UnixNano())
-	pixelgl.Run(run)
 }
 
 func loadPicture(path string) (pixel.Picture, error) {
