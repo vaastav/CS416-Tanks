a472674108a421d0ab1fb46e41c9823cd42d5042
Raunak Kumar <raunak.kumar17@outlook.com>
2018-04-01 09:59:07 -0700

    created client file for kv store

--- client/clocks.go
+++ client/clocks.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 	"net"
 	"net/rpc"
-	"path"
-	"path/filepath"
 	"proj2_f4u9a_g8z9a_i4x8_s8a9/crdtlib"
-	"strconv"
-	"strings"
 	"time"
 )
 
@@ -28,33 +23,6 @@ func (c *ClockController) SetOffset(offset time.Duration, ack *bool) error {
 // -----------------------------------------------------------------------------
 
 // KV: Get and Put functions.
-
-func ReadMapFromFile() (map[int]crdtlib.ValueType, error) {
-
-	M := make(map[int]crdtlib.ValueType)
-
-	files, err := filepath.Glob(path.Join(".", KVDir, "*.kv"))
-	if err != nil {
-		return M, err
-	}
-
-	for _, file := range files {
-		_, fname := path.Split(file)
-		fname = strings.Split(fname, ".")[0]
-		data, err := ioutil.ReadFile(file)
-		if err != nil {
-			return M, err
-		}
-		dataStr := strings.Split(string(data), "\n")
-		v0, _ := strconv.Atoi(dataStr[0])
-		v1, _ := strconv.Atoi(dataStr[1])
-		k, _ := strconv.Atoi(fname)
-		val := crdtlib.ValueType{v0, v1}
-		M[k] = val
-	}
-
-	return M, nil
-}
 
 func (c *ClockController) KVClientGet(key int, value *crdtlib.ValueType) error {
 
---
+++ client/kv.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"io/ioutil"
+	"path"
+	"path/filepath"
+	"proj2_f4u9a_g8z9a_i4x8_s8a9/crdtlib"
+	"strconv"
+	"strings"
+)
+
+func ReadMapFromFile() (map[int]crdtlib.ValueType, error) {
+
+	M := make(map[int]crdtlib.ValueType)
+
+	files, err := filepath.Glob(path.Join(".", KVDir, "*.kv"))
+	if err != nil {
+		return M, err
+	}
+
+	for _, file := range files {
+		_, fname := path.Split(file)
+		fname = strings.Split(fname, ".")[0]
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			return M, err
+		}
+		dataStr := strings.Split(string(data), "\n")
+		v0, _ := strconv.Atoi(dataStr[0])
+		v1, _ := strconv.Atoi(dataStr[1])
+		k, _ := strconv.Atoi(fname)
+		val := crdtlib.ValueType{v0, v1}
+		M[k] = val
+	}
+
+	return M, nil
+}
